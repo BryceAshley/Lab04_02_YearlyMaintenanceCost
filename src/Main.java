94c234365dc743486007eff1457b60b66c588b26
BryceAshley <110739176+BryceAshley@users.noreply.github.com>
2022-08-06 14:08:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    static int winterCosts = 450;
+    static int fallCosts = 250;
+    static int springCosts = 350;
+    static int summerCosts = 400;
+    static int yearlyCosts = 1450;
+
+    public static void main(String[] args) {
+        System.out.println("The total cost for winter is "+ winterCosts + " dollars.");
+        System.out.println("The total cost for fall is "+ fallCosts + " dollars.");
+        System.out.println("The total cost for spring is "+ springCosts + " dollars.");
+        System.out.println("The total cost for summer is "+ summerCosts + " dollars.");
+        System.out.println("The total cost for all the seasons together is "+ yearlyCosts + " dollars.");// write your code here
+    }
+}
